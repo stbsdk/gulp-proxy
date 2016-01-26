4889528ff93eb742895e76ca31f813a230714717
Stanislav Kalashnik <sk@infomir.eu>
2016-01-26 19:54:04 +0200

    rework config handling

--- config.js
+++ config.js
@@ -7,33 +7,35 @@
 
 'use strict';
 
-var extend = require('extend'),
-    config = require('spa-gulp/config');
+var extend   = require('extend'),
+    config   = require('spa-gulp/config'),
+    profiles = {};
 
 
-// base config
-// each profile inherits all options from the "default" profile
-module.exports = extend(true, {}, config, {
-    default: {
-        // listening HTTP port to serve proxy files
-        portHttp: 8800,
+// main
+profiles.default = extend(true, {}, config, {
+    // listening HTTP port to serve proxy files
+    portHttp: 8800,
 
-        // listening WebSocket port to serve requests
-        portWs: 8900,
+    // listening WebSocket port to serve requests
+    portWs: 8900,
 
-        // time between connection/sending attempts (in ms)
-        retryDelay: 100,
+    // time between connection/sending attempts (in ms)
+    retryDelay: 100,
 
-        // amount of connection/sending attempts before give up
-        retryLimit: 30,
+    // amount of connection/sending attempts before give up
+    retryLimit: 30,
 
-        // full logging
-        logging: false,
+    // full logging
+    logging: false,
 
-        // session name
-        name: 'anonymous',
+    // session name
+    name: 'anonymous',
 
-        // use localStorage to get/save requests data
-        cache: true
-    }
+    // use localStorage to get/save requests data
+    cache: true
 });
+
+
+// public
+module.exports = profiles;
